69e50b3f64260b648461d7a804d48d0c1348ab64
Isaac Avery <averyisaac9@gmail.com>
2026-03-09 12:10:44 -0600

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,18 @@
 
 
 int main() {
-  while(true){
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
+  while(true){
+  
   std::cout << "$ ";
 
   std::string userInput;
 
   std::cin >> userInput;
 
-  std::cout << userInput << ": command not found";
+  std::cout << userInput << ": command not found\n";
 }
 }
