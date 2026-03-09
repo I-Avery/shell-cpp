e7c476cb3b3b27ec4a431661c5151dd58ce0d2ba
Isaac Avery <averyisaac9@gmail.com>
2026-03-09 14:09:39 -0600

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,14 @@ int main() {
 
   std::cin >> userInput;
 
-  if (userInput == "exit")
-  {
+  // exiting the shell
+  if (userInput == "exit"){
     break;
-  } else {
+  } else if (userInput.substr(0,5) == "echo") {
+    std::cout << userInput.substr(5) << "\n";
+  } 
+
+  else {
     std::cout << userInput << ": command not found\n";
   }
 
