8137601cde65537b7f5966b9393469d93e2a451a
Isaac Avery <averyisaac9@gmail.com>
2026-03-10 08:33:59 -0600

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ int main() {
 
   // "echo" implementation
   if (userInput.substr(0,5) == "echo "){
-    std::cout << userInput.substr(6) << '\n';
+    std::cout << userInput.substr(5) << '\n';
   } else {
     std::cout << userInput << ": command not found\n";
     }
