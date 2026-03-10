2533b400a1e847a883e11d2d20fe948da0d26af7
Isaac Avery <averyisaac9@gmail.com>
2026-03-10 09:36:36 -0600

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,9 @@ int main() {
   // "echo" implementation
   if (userInput.substr(0,5) == "echo "){
     std::cout << userInput.substr(5) << '\n';
-  } 
+  } else {
+    std::cout << userInput << ": command not found\n";
+    }
 
   // "type" implementation
   if (userInput.substr(0,4) == "type"){
